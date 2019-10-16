99f2b4db6e823517bac8fb570685c4117b20863c
jjmato <jjmatotorres@gmail.com>
2019-10-16 19:27:44 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# example-angular-material-reactive-form-en2usp
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/example-angular-material-reactive-form-en2usp)
---
+++ app/app.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { ReactiveFormsModule } from "@angular/forms";
+
+import { MaterialModule } from "./material.module";
+import { AppComponent } from "./app.component";
+
+import { MAT_FORM_FIELD_DEFAULT_OPTIONS } from "@angular/material";
+
+@NgModule({
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    ReactiveFormsModule,
+    MaterialModule
+  ],
+  declarations: [AppComponent],
+  bootstrap: [AppComponent],
+  providers: [
+    {
+      provide: MAT_FORM_FIELD_DEFAULT_OPTIONS,
+      useValue: { appearance: "outline" }
+    }
+  ]
+})
+export class AppModule {}
